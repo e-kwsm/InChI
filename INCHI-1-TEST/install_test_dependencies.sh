985a217d6974a4e4e658d66bbd8443d969da3a2c
Christoph Mueller <cmueller@beilstein-institut.de>
2025-08-19 09:37:36 +0000

    Added googletest clone to test dependencies

--- INCHI-1-TEST/install_test_dependencies.sh
+++ INCHI-1-TEST/install_test_dependencies.sh
@@ -2,3 +2,7 @@
 
 python -m pip install --upgrade --break-system-packages pip
 python -m pip install --break-system-packages -e .[invariance-tests]
+
+
+git clone https://www.github.com/google/googletest
+
