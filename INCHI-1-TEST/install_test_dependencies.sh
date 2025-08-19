72f1563edde1517b2f0c5770f558254e47905142
Christoph Mueller <cmueller@beilstein-institut.de>
2025-08-19 13:27:19 +0000

    Unzip folder fix

--- INCHI-1-TEST/install_test_dependencies.sh
+++ INCHI-1-TEST/install_test_dependencies.sh
@@ -5,4 +5,5 @@ python -m pip install --break-system-packages -e .[invariance-tests]
 
 #Install Googletest library 
 wget -O googletest.zip https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
-unzip googletest.zip -d ../googletest && rm googletest.zip
+unzip googletest.zip -d .. && mv ../googletest-03597a01ee50ed33e9dfd640b249b4be3799d395 ../googletest && rm googletest.zip
+
