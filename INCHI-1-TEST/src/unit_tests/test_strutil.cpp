cf60aa209d06875c1f330527bae6ca7ad5080d01
Christoph Mueller <cmueller@beilstein-institut.de>
2025-08-19 09:29:32 +0000

    Added first unit test for strutil.c

---
+++ INCHI-1-TEST/src/unit_tests/test_strutil.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+extern "C"
+{
+#include "../../../INCHI-1-SRC/INCHI_BASE/src/strutil.h"
+}
+
+TEST(strutil_testing, test_SetConnectedComponentNumber)
+{
+
+    int num_atoms = 1;
+    int test_component_number = 23;
+    inp_ATOM *new_mol = CreateInpAtom(num_atoms);
+
+    // int SetConnectedComponentNumber( inp_ATOM *at, int num_at, int component_number )
+    EXPECT_EQ(SetConnectedComponentNumber(new_mol, 1, test_component_number), 0);
+
+    for (int i = 0; i < num_atoms; i++)
+    {
+        EXPECT_EQ(new_mol[i].component, test_component_number);
+    }
+
+    // EXPECT_STREQ(szElement, "??");
+
+    FreeInpAtom(&new_mol);
+}
