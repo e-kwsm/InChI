40c9f073854e987705fe7670cec47b478d191def
Christoph Mueller <cmueller@beilstein-institut.de>
2025-11-28 14:39:56 +0000

    Changed comments for sterac, steabs, sterac, added unit tests to check whether sterac, steabs, sterac info is transferred to orig_atom_data

--- INCHI-1-TEST/tests/test_unit/test_mol_fmt.cpp
+++ INCHI-1-TEST/tests/test_unit/test_mol_fmt.cpp
@@ -375,7 +375,7 @@ TEST(mol_fmt_testing, test_ReadMolfile_v3000)
         "  ChemDraw11272515282D\n"
         "\n"
         "  0  0  0     0  0              0 V3000\n"
-        "M  V30 BEGIN CTAB\n"
+        "M  V30 BEGIN CTAB    \n"
         "M  V30 COUNTS 24 25 0 0 0\n"
         "M  V30 BEGIN ATOM     \n"
         "M  V30 1 O -0.309899 1.774158 0.000000 0\n"
@@ -487,4 +487,155 @@ TEST(mol_fmt_testing, test_ReadMolfile_v3000)
 
     EXPECT_EQ(result->ctab.n_atoms, 24);
     EXPECT_EQ(result->ctab.n_bonds, 25);
+}
+
+TEST(mol_fmt_testing, test_ReadMolfile_v3000_sgroup)
+{
+
+    INCHI_IOSTREAM input_stream;
+
+    const char *molblock =
+        "enhanc_stereo1									  \n"
+        "  ACD/LABS08242216132D                            \n"
+        "												  \n"
+        "  0  0  0  0  0  0  0  0  0  0999 V3000           \n"
+        "M  V30 BEGIN CTAB                                 \n"
+        "M  V30 COUNTS 18 17 0 0 1                         \n"
+        "M  V30 BEGIN ATOM                                 \n"
+        "M  V30 1 C 3424.1946 -1936.7935 0 0               \n"
+        "M  V30 2 C 3352.3145 -1895.2935 0 0               \n"
+        "M  V30 3 C 3280.4346 -1936.7935 0 0               \n"
+        "M  V30 4 C 3208.5542 -1895.2935 0 0               \n"
+        "M  V30 5 C 3136.6743 -1936.7935 0 0               \n"
+        "M  V30 6 C 3064.7944 -1895.2935 0 0               \n"
+        "M  V30 7 Br 3136.6743 -2019.7935 0 0              \n"
+        "M  V30 8 Cl 3208.5542 -1812.2935 0 0              \n"
+        "M  V30 9 Cl 3280.4346 -2019.7935 0 0              \n"
+        "M  V30 10 Cl 3352.3145 -1812.2935 0 0             \n"
+        "M  V30 11 Cl 3424.1946 -2019.7935 0 0             \n"
+        "M  V30 12 C 3496.075 -1895.2935 0 0               \n"
+        "M  V30 13 C 3567.9548 -1936.7942 0 0              \n"
+        "M  V30 14 C 3639.835 -1895.2944 0 0               \n"
+        "M  V30 15 C 3711.7148 -1936.7942 0 0              \n"
+        "M  V30 16 Cl 3639.835 -1812.2944 0 0              \n"
+        "M  V30 17 Cl 3567.9548 -2019.7942 0 0             \n"
+        "M  V30 18 Cl 3496.075 -1812.2937 0 0              \n"
+        "M  V30 END ATOM                                   \n"
+        "M  V30 BEGIN BOND                                 \n"
+        "M  V30 1 1 1 2                                    \n"
+        "M  V30 2 1 1 11 CFG=3                             \n"
+        "M  V30 3 1 1 12                                   \n"
+        "M  V30 4 1 2 3                                    \n"
+        "M  V30 5 1 2 10 CFG=1                             \n"
+        "M  V30 6 1 3 4                                    \n"
+        "M  V30 7 1 3 9 CFG=1                              \n"
+        "M  V30 8 1 4 5                                    \n"
+        "M  V30 9 1 4 8 CFG=1                              \n"
+        "M  V30 10 1 5 6                                   \n"
+        "M  V30 11 1 5 7 CFG=1                             \n"
+        "M  V30 12 1 12 13                                 \n"
+        "M  V30 13 1 12 18 CFG=3                           \n"
+        "M  V30 14 1 13 14                                 \n"
+        "M  V30 15 1 13 17 CFG=1                           \n"
+        "M  V30 16 1 14 15                                 \n"
+        "M  V30 17 1 14 16 CFG=1                           \n"
+        "M  V30 END BOND                                   \n"
+        "M  V30 BEGIN COLLECTION                           \n"
+        "M  V30 MDLV30/STERAC2 ATOMS=(1 1)                 \n"
+        "M  V30 MDLV30/STERAC1 ATOMS=(2 2 3)               \n"
+        "M  V30 MDLV30/STEABS ATOMS=(2 4 5)                \n"
+        "M  V30 MDLV30/STEREL1 ATOMS=(2 12 13)             \n"
+        "M  V30 MDLV30/STEREL2 ATOMS=(1 14)                \n"
+        "M  V30 END COLLECTION                             \n"
+        "M  V30 END CTAB                                   \n"
+        "M  END                                            \n";
+
+    inchi_ios_init(&input_stream, INCHI_IOS_TYPE_STRING, nullptr);
+    inchi_ios_print_nodisplay(&input_stream, molblock);
+
+    // MOL_FMT_DATA *ReadMolfile(INCHI_IOSTREAM *inp_file,
+    //                         MOL_FMT_HEADER_BLOCK *OnlyHeaderBlock,
+    //                         MOL_FMT_CTAB *OnlyCTab,
+    //                         int bGetOrigCoord,
+    //                         int treat_polymers,
+    //                         int treat_NPZz,
+    //                         char *pname,
+    //                         int lname,
+    //                         unsigned long *Id,
+    //                         const char *pSdfLabel,
+    //                         char *pSdfValue,
+    //                         int *err,
+    //                         char *pStrErr,
+    //                         int bNoWarnings)
+
+    INCHI_IOSTREAM *inp_file = &input_stream;
+    MOL_FMT_HEADER_BLOCK *OnlyHeaderBlock = nullptr;
+    MOL_FMT_CTAB *OnlyCTab = nullptr;
+    int bGetOrigCoord = 0;
+    int treat_polymers = 0;
+    int treat_NPZz = 0;
+    char *pname = nullptr;
+    int lname = 0;
+    unsigned long *Id = nullptr;
+    const char *pSdfLabel = nullptr;
+    char *pSdfValue = nullptr;
+    int err = 0; // nullptr;
+    char *pStrErr = nullptr;
+    int bNoWarnings = 0;
+
+    MOL_FMT_DATA *result = ReadMolfile(
+        inp_file,
+        OnlyHeaderBlock,
+        OnlyCTab,
+        bGetOrigCoord,
+        treat_polymers,
+        treat_NPZz,
+        pname,
+        lname,
+        Id,
+        pSdfLabel,
+        pSdfValue,
+        &err,
+        pStrErr,
+        bNoWarnings);
+
+    EXPECT_STREQ(result->hdr.prog_name, "ACD/LABS");
+    EXPECT_STREQ(result->hdr.molname, "enhanc_stereo1");
+
+    EXPECT_EQ(result->ctab.n_atoms, 18);
+    EXPECT_EQ(result->ctab.n_bonds, 17);
+
+    EXPECT_EQ(result->ctab.v3000->n_steabs, 1);
+
+    // STEABS ATOMS=(2 4 5)
+    EXPECT_EQ(result->ctab.v3000->steabs->lists[0][0], 1); // - not used
+    EXPECT_EQ(result->ctab.v3000->steabs->lists[0][1], 2); // number of members in collection
+    EXPECT_EQ(result->ctab.v3000->steabs->lists[0][2], 4); //  member atom numbers
+    EXPECT_EQ(result->ctab.v3000->steabs->lists[0][3], 5); //  member atom numbers
+
+    // STERAC2 ATOMS=(1 1)
+    EXPECT_EQ(result->ctab.v3000->n_sterac, 2);
+
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[0][0], 2); // n from "STERACn" tag
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[0][1], 1); // number of members in collection
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[0][2], 1); // member atom numbers
+
+    // STERAC1 ATOMS=(2 2 3)
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[1][0], 1); // STERAC1 ATOMS=(2 2 3)
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[1][1], 2); // number of members in collection
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[1][2], 2); // member atom numbers
+    EXPECT_EQ(result->ctab.v3000->sterac->lists[1][3], 3); // member atom numbers
+
+    EXPECT_EQ(result->ctab.v3000->n_sterel, 2);
+
+    // STEREL1 ATOMS=(2 12 13)
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[0][0], 1);  // n from "STERELn" tag
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[0][1], 2);  // number of members in collection
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[0][2], 12); // member atom numbers
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[0][3], 13); // member atom numbers
+
+    // STEREL2 ATOMS=(1 14)
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[1][0], 2);  // n from "STERELn" tag
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[1][1], 1);  // number of members in collection
+    EXPECT_EQ(result->ctab.v3000->sterel->lists[1][2], 14); // member atom numbers
 }
