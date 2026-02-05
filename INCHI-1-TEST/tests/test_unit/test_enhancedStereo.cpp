cf6d7ce3c5de92fe4382f70875783192b760c629
Christoph Mueller <cmueller@beilstein-institut.de>
2026-02-05 14:10:07 +0000

    fixed unit test for atropisomer in enh stereo

--- INCHI-1-TEST/tests/test_unit/test_enhancedStereo.cpp
+++ INCHI-1-TEST/tests/test_unit/test_enhancedStereo.cpp
@@ -179,7 +179,7 @@ TEST(test_enhancedStereo, test_EnhancedStereochemistry_2)
     char options[] = "-EnhancedStereochemistry";
     inchi_Output output;
     inchi_Output *poutput = &output;
-    const char expected_inchi[] = "InChI=1B/C10H14BrCl7/c1-3(11)5(13)7(15)9(17)10(18)8(16)6(14)4(2)12/h3-10H,1-2H3/t3-,4-,5+,6-,7-,8-,9+,10-/m0/s1(3,5)2(6,8)(4)3(7,9)(10)";
+    const char expected_inchi[] = "InChI=1B/C12H6Br2Cl2/c13-7-3-1-5-9(15)11(7)12-8(14)4-2-6-10(12)16/h1-6H";
 
     EXPECT_EQ(MakeINCHIFromMolfileText(molblock, options, poutput), 0);
     EXPECT_STREQ(poutput->szInChI, expected_inchi);
